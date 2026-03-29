7ab9ac3be718a241bcb2f9c15daa253de349b6ce
Sunnywslau <sunnywslau@gmail.com>
2026-03-29 03:13:25 +0000

    Update taf_functions.py

--- taf_functions.py
+++ taf_functions.py
@@ -100,7 +100,8 @@ def highlight_taf(taf_text):
     # Vertical Visibility / Unmeasured
     unmeasured_pattern = r'\b(VV///|VV\d{3})\b'
     # Freezing conditions: FZ anywhere as a weather group (e.g. -FZDZ, FZRA)
-    freezing_pattern = r'(?<!\S)(\S*?FZ[A-Z]*)(?!\S)'
+    #freezing_pattern = r'(?<!\S)(\S*?FZ[A-Z]*)(?!\S)'  
+    freezing_pattern = r'(?<!\S)([-+]?[A-Z]*FZ[A-Z]*)(?!\S)'
     # Snow: SN anywhere as a weather group (e.g. -SN, BLSN, SNRA)
     snow_pattern = r'(?<!\S)(\S*?SN[A-Z]*)(?!\S)'
 
